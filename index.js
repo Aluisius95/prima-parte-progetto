eabdb6abe43d25b678c789539c1b104edd22c0cb
Aluisius95 <120209413+Aluisius95@users.noreply.github.com>
2023-07-16 20:42:59 +0200

    2 files modified

--- index.js
+++ index.js
@@ -4,22 +4,17 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>Cerca libro per titolo</h1>`;
-const searchB = document.getElementById('search');
+var apiKey;
+let URL;
+const searchB = document.getElementById('searchB');
 const result = document.getElementById('answer');
-const apiKey = '77a2224b';
-const URL =
-  'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint/get?key=' +
-  apiKey;
-
-//aggiungo casella di testo
-let casella = document.createElement('input');
-casella.setAttribute('type', 'text');
-searchB.appendChild(casella);
 
 //genero una request HTML
 function newReq(title, callback) {
   const request = new XMLHttpRequest();
-
+  URL =
+    'https://eu-central-1.aws.data.mongodb-api.com/app/kvaas-giwjg/endpoint/get?key=' +
+    apiKey;
   request.onload = fetch(URL)
     .then(
       (response) => response.json(),
@@ -33,16 +28,18 @@ function newReq(title, callback) {
 
 //funzione di chiamata request e match stringa
 function searchRB(title) {
+  apiKey = document.getElementById('api').value;
   result.innerHTML = '';
-  if(title.length == 0) result.innerHTML = '';
-  else newReq(title, (c) => {
-    let reg = new RegExp(title, 'i');
-    c.forEach((element) => {
-      if (element['titolo'].search(reg) != -1) {
-        result.innerHTML += element.autore + ': ' + element.titolo + '.<br>';
-      }
+  if (title.length == 0) result.innerHTML = '';
+  else
+    newReq(title, (c) => {
+      let reg = new RegExp(title, 'i');
+      c.forEach((element) => {
+        if (element['titolo'].search(reg) != -1) {
+          result.innerHTML += element.autore + ': ' + element.titolo + '.<br>';
+        }
+      });
     });
-  });
 }
 
-casella.addEventListener('input', () => searchRB(casella.value));
+searchB.addEventListener('input', () => searchRB(searchB.value));
